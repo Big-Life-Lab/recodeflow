8c9724dfb96533b1c7ddb8e33f3a32a639422bd5
lurn <lukebaileyx@gmail.com>
2020-08-31 21:24:59 -0400

    [Refactor] Always use double quotes

--- R/env.R
+++ R/env.R
@@ -1,16 +1,16 @@
 pkg.env <- new.env(parent = emptyenv())
 
-pkg.env$NA_invalid <- 'NA::a'
-pkg.env$NA_missing <- 'NA::b'
+pkg.env$NA_invalid <- "NA::a"
+pkg.env$NA_missing <- "NA::b"
 pkg.env$all_NAs <- c(pkg.env$NA_invalid, pkg.env$NA_missing)
 
-pkg.env$missing <- 'missing'
+pkg.env$missing <- "missing"
 
-pkg.env$db_var_start_infix <- '::'
+pkg.env$db_var_start_infix <- "::"
 
-pkg.env$rec_to_copy <- 'copy'
+pkg.env$rec_to_copy <- "copy"
 
-pkg.env$var_details_cat <- 'cat'
-pkg.env$var_details_cont <- 'cont'
+pkg.env$var_details_cat <- "cat"
+pkg.env$var_details_cont <- "cont"
 
-pkg.env$var_cat <- 'Categorical'
+pkg.env$var_cat <- "Categorical"
