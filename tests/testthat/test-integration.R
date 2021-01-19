294b7ee241032e721aa5dbda71d779c0c16a2e96
lurn <lukebaileyx@gmail.com>
2021-01-19 10:13:02 -0500

    [Bug] Fix issue where strings in files may be read as factors

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -8,8 +8,8 @@ test_that("The PMML file is correctly generated", {
     file.info(expected_pmml_file)$size
   ))
 
-  var_details_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variable_details.csv")
-  vars_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variables.csv")
+  var_details_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variable_details.csv", stringsAsFactors = FALSE)
+  vars_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variables.csv", stringsAsFactors = FALSE)
   db_name <- "cchs2001_p"
   vars <- c("ADL_01", "ADL_02", "ADL_05", "ALW_2A1")
 
