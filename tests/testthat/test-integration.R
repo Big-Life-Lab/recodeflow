4b5cde1e0d6ebdc3e538d89d52c670a3bdbd138c
lurn <lukebaileyx@gmail.com>
2021-01-19 14:57:45 -0500

    [Bug] Fix issue where different newline sequences could break tests

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -3,10 +3,11 @@ library("XML")
 
 test_that("The PMML file is correctly generated", {
   expected_pmml_file <- "../../assets/tests/integration/expected-pmml.xml"
-  expected_pmml_string <- trimws(readChar(
+  expected_pmml_string_lines <- readLines(
     expected_pmml_file,
     file.info(expected_pmml_file)$size
-  ))
+  )
+  expected_pmml_string <- paste(expected_pmml_string_lines, collapse = "\n")
 
   var_details_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variable_details.csv", stringsAsFactors = FALSE)
   vars_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variables.csv", stringsAsFactors = FALSE)
