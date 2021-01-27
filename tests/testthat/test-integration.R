6cef1098cb234d0a77cb5a25cc634baae3c4713d
Yulric Sequeira <yulrics@gmail.com>
2021-01-26 19:01:07 -0500

    Include fileEncoding arg when reading in variables details for tests to avoid a "i..." being added to the first column. Updated the vars to convert for the test.

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -9,10 +9,12 @@ test_that("The PMML file is correctly generated", {
   )
   expected_pmml_string <- paste(expected_pmml_string_lines, collapse = "\n")
 
-  var_details_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variable_details.csv", stringsAsFactors = FALSE)
+  var_details_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variable_details.csv",
+                                stringsAsFactors = FALSE,
+                                fileEncoding = "UTF-8-BOM")
   vars_sheet <- read.csv("../../assets/tests/integration/cchsflow MSW - variables.csv", stringsAsFactors = FALSE)
   db_name <- "cchs2001_p"
-  vars <- c("ADL_01", "ADL_02", "ADL_05", "ALW_2A1")
+  vars <- c("ADL_01", "ALW_2A1", "DHHGAGE_cont")
 
   actual_pmml <- xflow_to_pmml(
     var_details_sheet,
