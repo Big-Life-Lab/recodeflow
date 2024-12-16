5d69c8d05cc34e61b6912bcb381c89bc62cb95bc
Yulric Sequeira <yulrics@gmail.com>
2024-12-16 13:49:51 -0500

    Added documentation for tester variables and variable details sheet so R CMD check does not complain

--- R/data.R
+++ R/data.R
@@ -86,3 +86,44 @@
 #' \item{variableStartLabel}{variable start label}
 #' }
 "pbc_variable_details"
+
+#' Example variables sheet for vignettes
+#'
+#' @format A data frame with 24 rows and 11 columns:
+#' \describe{
+#' \item{variable}{variable name}
+#' \item{label}{variable label}
+#' \item{labelLong}{variable label long}
+#' \item{subject}{subject}
+#' \item{section}{section}
+#' \item{variableType}{variable type}
+#' \item{databaseStart}{database start}
+#' \item{units}{units}
+#' \item{variableStart}{variable start}
+#' \item{notes}{logical indicating presence of notes}
+#' \item{description}{logical indicating presence of description}
+#' }
+"tester_variables"
+
+#' Example variable details sheet for vignettes
+#'
+#' @format A data frame with 69 rows and 16 columns:
+#' \describe{
+#' \item{variable}{variable name}
+#' \item{dummyVariable}{dummy variable name}
+#' \item{typeEnd}{end type}
+#' \item{databaseStart}{database start}
+#' \item{variableStart}{variable start}
+#' \item{typeStart}{start type}
+#' \item{recEnd}{record end}
+#' \item{recStart}{record start}
+#' \item{catLabel}{category label}
+#' \item{catLabelLong}{category long label}
+#' \item{numValidCat}{number of valid categories (numeric)}
+#' \item{units}{logical indicating presence of units}
+#' \item{notes}{logical indicating presence of notes}
+#' \item{catStartLabel}{category start label}
+#' \item{variableStartShortLabel}{variable start short label}
+#' \item{variableStartLabel}{variable start label}
+#' }
+"tester_variable_details"
