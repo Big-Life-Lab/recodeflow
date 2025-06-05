e5cac4f1f6fb8fba1b01a563280d0a406f1aa30b
Yulric Sequeira <yulrics@gmail.com>
2025-06-05 10:28:09 -0400

    display snapshot comparison when integration test fails

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,7 +1,27 @@
+snapshot_file_name <- 'integration.RData'
+
 write_temp_rdata <- function(x) {
   path <- tempfile(fileext = ".RData")
   save(x, file = path)
   return(path)
+}
+
+get_snapshot_file_path <- function(file_name) {
+  return(file.path('./_snaps/integration/', file_name))
+}
+
+compare_snapshot <- function() {
+  env_load <- new.env()
+
+  expected_data_name <- load(
+    get_snapshot_file_path(snapshot_file_name), envir = env_load)
+  expected_data <- get(expected_data_name, env_load)
+
+  actual_data_name <- load(
+    get_snapshot_file_path('integration.new.RData'), envir = env_load)
+  actual_data <- get(actual_data_name, env_load)
+
+  print(waldo::compare(expected_data, actual_data))
 }
 
 # This test was taken from the HUIPoRT project and uses the development
@@ -34,5 +54,17 @@ test_that("Integration test", {
     }) %>%
     purrr::list_rbind()
 
-  expect_snapshot_file(write_temp_rdata(actual_data), "integration.RData")
+  # The following code fixes the issue that testthat does not have an in-built
+  # way to diff .RData files.
+  tryCatch({
+      expect_snapshot_file(
+        write_temp_rdata(actual_data),
+        snapshot_file_name
+      )
+    },
+    error = function(e) {
+      compare_snapshot()
+      stop(e)
+    }
+  )
 })
